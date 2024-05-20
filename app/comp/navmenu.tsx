9cc735c241ee9a26b0b840b69193667c39077530
sudoR3D <cbshourov@gmail.com>
2024-05-21 02:46:11 +0600

    newtest

--- app/comp/navmenu.tsx
+++ app/comp/navmenu.tsx
@@ -2,11 +2,11 @@ import Loading from "./loading"
 import { useNavigation } from "@remix-run/react"
 export default function NavMenu() {
     return (
-        <nav className="z-50 fixed top-0 w-full 3x1 font-medium justify-center flex items-center py-3 px-8 bg-zinc-900 shadow-md shadow-zinc-900">
+        <nav className="z-50 fixed top-0 w-full font-medium justify-center flex items-center px-8 bg-neutral-900 shadow-md shadow-zinc-900 pb-1">
             <div className="w-full md:w-[704px] xl:w-[820px] lg:w-[820px]">
-                <div className="transition duration-100 hover:scale-[1.05] inline-flex text-3xl justify-center align-middle cursor-pointer items-center">
-                    <img src="/logo.webp" alt="RedNotes" className=" h-[44px] w-[44px] align-middle" />
-                    <h1 className=" font-Kayak text-accent tracking-normal content-center pl-3">RedNotes</h1>
+                <div className="transition duration-100 hover:text-accent hover:scale-[1.05] text-red-600 inline-flex text-[2.5rem] justify-center align-middle cursor-pointer items-center">
+                    <img src="/logo.webp" alt="RedNotes" className=" h-[52px] w-[52px] align-middle" />
+                    <h1 className="font-Kayak tracking-wide pt-1 content-center pl-3">RedNotes</h1>
                     {useNavigation().state === "loading" ?
                         <Loading /> : null}
                 </div>
--- app/comp/notelist.tsx
+++ app/comp/notelist.tsx
@@ -50,19 +50,19 @@ const NoteList = [{
 import { Link, redirect } from "@remix-run/react";
 export default function Notetiles() {
     return (
-        <div className="mx-8 mb-7 mt-[5rem] grid justify-center text-zinc-300">
+        <div className="mx-8 mb-7 mt-[5.5rem] grid justify-center text-zinc-300">
             {NoteList.map(note => (
                 <div className="md:w-[704px] xl:w-[820px] lg:w-[820px] hover:scale-[1.01] hover:duration-150">
                     <div className="flex my-1.5">
                         <form className="inline-flex items-center" action="/" method="post">
                             <input type="hidden" value={note.noteid} />
                             {note.isStared ?
-                                <button type="submit" className="p-3.5 bg-accent hover:bg-red-600 transition hover:duration-100 group rounded-l-lg">
+                                <button type="submit" className="p-3.5 bg-accent hover:bg-red-700 transition hover:duration-100 group rounded-l-lg">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="w-6 h-6 fill-white">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                                     </svg>
                                 </button> :
-                                <button type="submit" className="p-3.5 bg-gray-900 hover:bg-red-600 transition hover:duration-100 group rounded-l-lg">
+                                <button type="submit" className="p-3.5 bg-gray-900 hover:bg-red-700 transition hover:duration-100 group rounded-l-lg">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="w-6 h-6 group-hover:fill-white">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                                     </svg>
